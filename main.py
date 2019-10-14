de469c2a0824d88795bfd8abdfa1ce703efe26fd
Julian <julian.oquendogr@gmail.com>
2019-10-14 14:04:35 -0500

    It validates only jpg format to be changed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-
 import os
 folder = 'E:\\Users\\JULIAN\\Pictures\\TESORO'
 path = os.path.realpath(folder)
 os.startfile(path)
 Cont = 0
-Format = ".JPG"
+UpperFormat = ".JPG"
+LowFormart = ".jpg"
+Contain = ""
 
 print("Ingrese nombre para los archivos")
 NewName = input()
@@ -12,9 +13,15 @@ NewName = NewName.upper()
 
 with os.scandir(folder) as ListFolder:
     for file in ListFolder:
-        Cont = Cont + 1
-        os.rename(folder + "\\" + file.name , 
-                  folder + "\\"  + NewName + "_" + str(Cont) + Format)
-
-print("Se han cambiado los nombres de la carpeta :" + folder)
+            Contain = file.name
+            Contain = Contain.upper()
+            if UpperFormat in Contain:
+                Cont = Cont + 1
+                os.rename(folder + "\\" + file.name , 
+                        folder + "\\"  + NewName + "_" + str(Cont) + UpperFormat)
+            else:
+                print("Formato no valido: " + Contain)
+    else:
+        pass
+        print("Se han cambiado los nombres de la carpeta :" + folder)
 
