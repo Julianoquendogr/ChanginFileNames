088c968aa88399027e4f019d0e3fed6efb274aba
Julian <julian.oquendogr@gmail.com>
2019-10-14 13:16:19 -0500

    firt commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import os
+folder = 'E:\\Users\\JULIAN\\Pictures\\TESORO'
+path = os.path.realpath(folder)
+os.startfile(path)
+Cont = 0
+Format = ".JPG"
+
+print("Ingrese nombre para los archivos")
+NewName = input()
+NewName = NewName.upper()   
+
+with os.scandir(folder) as ListFolder:
+    for file in ListFolder:
+        Cont = Cont + 1
+        os.rename(folder + "\\" + file.name , 
+                  folder + "\\"  + NewName + "_" + str(Cont) + Format)
+
+print("Se han cambiado los nombres de la carpeta :" + folder)
