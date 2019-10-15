d1afdb4179ec135d32dbc40de1876a8c28208616
Julian <julian.oquendogr@gmail.com>
2019-10-14 22:55:58 -0500

    Adding timestamp to avoid duplicate .txt files

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from datetime import datetime
 import os
 folder = 'E:\\Users\\JULIAN\\Pictures\\TESORO'
 Cont = 0
 UpperFormat = ".JPG"
 Contain = ""
+time = datetime.now()
+timestamp = str(time.year)+str(time.month)+str(time.day)+str(time.hour)+str(time.minute)+str(time.second)
 
 print("Ingrese nombre para los archivos")
 NewName = input()
 NewName = NewName.upper()
-txtFile = open("No_Cambiados.txt","w+")
+txtFile = open("No_Cambiados_" + timestamp + ".TXT", "w+")
 
 with os.scandir(folder) as ListFolder:
     for file in ListFolder:
@@ -19,11 +22,11 @@ with os.scandir(folder) as ListFolder:
                           folder + "\\" + NewName + "_" + str(Cont) + UpperFormat)
             else:
                 print("Formato no valido: " + Contain)
-                txtFile.write(Contain +'\n')
+                txtFile.write(Contain + '\n')
     else:
         pass
         print("Se han cambiado los nombres de la carpeta :" + folder)
-    
+
 path = os.path.realpath(folder)
 os.startfile(path)
 txtFile.close()
