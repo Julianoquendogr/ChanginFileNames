8cc9a1227699c5658f2e172d61878292ff730f85
Julian <julian.oquendogr@gmail.com>
2019-10-19 00:05:04 -0500

    Code added to avoid write in txt file its itself name, 'try' if same name file comes.

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import os
 
 #Give option to open any directory and do file changes
 folder = askdirectory()
-folder = folder.replace("/","\\\\")
+folder = folder.replace("/", "\\\\")
 
 Cont = 0
 UpperFormat = ".JPG"
 Contain = ""
 FileName = "OtroFormato_"
-CompleteName = os.path.join(folder,FileName)
+CompleteName = os.path.join(folder, FileName)
 
 #creating timestamp to add to the file name in order to not create duplicates files
 time = datetime.now()
-timestamp = str(time.year)+str(time.month)+str(time.day)+str(time.hour)+str(time.minute)+str(time.second)
+timestamp = str(time.year)+str(time.month)+str(time.day) + \
+    str(time.hour)+str(time.minute)+str(time.second)
 
 #creates the file
 txtFile = open(CompleteName + timestamp + ".txt", "w+")
 
-print("Ingrese nombre para los archivos")
+print("Type name to rename files")
 #Convert to upperCase
 NewName = input()
 NewName = NewName.upper()
@@ -30,16 +31,20 @@ with os.scandir(folder) as ListFolder:
             Contain = Contain.upper()
             if UpperFormat in Contain:
                 Cont = Cont + 1
-                os.rename(folder + "\\" + file.name,
-                          folder + "\\" + NewName + "_" + str(Cont) + UpperFormat)
+                try:
+                    os.rename(folder + "\\" + file.name, folder +
+                              "\\" + NewName + "_" + str(Cont) + UpperFormat)
+                except OSError:
+                     print("Something went wrong with : " + NewName)
+            elif FileName.upper() in Contain:
+                    pass
             else:
-                print("Formato no valido: " + Contain)
+                print("Format is not valid: " + Contain)
                 txtFile.write(Contain + '\n')
     else:
         pass
-        print("Se han cambiado los nombres de la carpeta : " + folder)
+        print("Python has changed files in selected path : " + folder)
 
 path = os.path.realpath(folder)
 os.startfile(path)
 txtFile.close()
-
